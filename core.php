1cede34c40d2d437b414ab5c675dfb8da3f1fe86
ratchaluk <man_tep@hotmail.com>
2020-03-11 14:51:01 +0700

    Add Division funtion in php

--- core.php
+++ core.php
@@ -1,5 +1,13 @@
 <?php
-
-
+	function Division($a,$b){
+            
+            if($a==0 or $b ==0){
+                return "No!!! Don't Input Number 0";
+            }
+            else{
+                return ($b/$a);
+            }
+	}
+	echo Division(4,2);
 ?>
 
