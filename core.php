e4c85fe5973a6fb71a367e4c1d700aae2dc0b523
WarisDev <warissara13@gmail.com>
2020-03-11 14:46:52 +0700

    add sqrt to core.php

--- core.php
+++ core.php
@@ -1,5 +1,15 @@
 <?php
 
+<h>--SquareRoot--</h>
+<input id="inpN" type="text" oninput="SRoot(this.value)" onchange="SRoot(this.value)">
+<p>resault: <span id="SRoot"></span></p>
+
+<script>
+function SRoot(num){
+    document.getElementById("SRoot").innerHTML = Math.sqrt(num); 
+}
+</script>
+
 
 ?>
 
